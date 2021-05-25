bbdeec28ad9dc7d53ce4eb0a7b9ef3e19627f09b
William Desportes <williamdes@wdes.fr>
2021-05-25 12:36:01 +0200

    Fix sidebar heading

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,7 +1,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php $this->route('/'); ?>">
         <div class="sidebar-brand-text mx-3"><?php $this->value('site_name'); ?></div>
     </a>
     <!-- Divider -->
